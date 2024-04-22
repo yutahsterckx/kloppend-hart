e6d2f36392afa209c2c0b23485071cd56cc44bd0
yutahsterckx <167744748+yutahsterckx@users.noreply.github.com>
2024-04-22 09:20:24 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.forever(function () {
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
